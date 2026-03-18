3ca739a31b67fd8ff82a0c1102849d15ebdbdd0d
livecoderuaridh <ruaridhbell73@googlemail.com>
2026-03-18 22:48:20 +0000

    updates to 33mm

--- index.js
+++ index.js
@@ -5,102 +5,88 @@ const { Client, Intents, MessageEmbed } = require("discord.js");
 let previouslyInStock = [];
 
 const client = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS, //adds server functionality
-    Intents.FLAGS.GUILD_MESSAGES, //gets messages from our bot.
-  ],
+    intents: [
+        Intents.FLAGS.GUILDS, //adds server functionality
+        Intents.FLAGS.GUILD_MESSAGES, //gets messages from our bot.
+    ],
 });
 
 client.once("ready", async () => {
-  console.log("Ready!");
-  await main();
+    console.log("Ready!");
+    await main();
 });
 
 function delay(delayInms) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(2);
-    }, delayInms);
-  });
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(2);
+        }, delayInms);
+    });
 }
 
 async function main() {
-  while (true) {
-    await checkAvaliable(6720326140036, 40013517029508);
-    await delay(8000);
-    await checkAvaliable(6720326140036, 40013517062276);
-    await delay(8000);
-    await checkAvaliable(5141773779076, 34519185686660);
-    await delay(8000);
-    await checkAvaliable(5141773779076, 34519185719428);
-    await delay(300000);
-  }
+    while (true) {
+        await checkAvaliable(8999808401620, 47293250928852);
+        await delay(8000);
+        await checkAvaliable(8999808401620, 47293250961620);
+        await delay(300000);
+    }
 }
 
 async function checkAvaliable(productID, sizeID) {
-  let res, index;
+    let res;
 
-  let randomNumber = Math.floor(Math.random() * 100000);
+    let randomNumber = Math.floor(Math.random() * 100000);
 
-  try {
-    res = await got.get(
-      `https://threadbare.com/collections/mens-trousers/products.json?limit=${randomNumber}`
-    );
+    try {
+        res = await got.get(
+            `https://www.33-mm.com/collections/knitwear/products.json?limit=${randomNumber}`,
+        );
 
-    const obj = JSON.parse(res.body);
-    let product = await obj.products.find((el) => el.id === productID);
+        const obj = JSON.parse(res.body);
 
-    let size = await product.variants.find((el) => el.id === sizeID);
-    let test = await previouslyInStock.find((el) => el.id === sizeID);
-    // console.log(test);
+        let product = await obj.products.find((el) => el.id === productID);
 
-    if (test != "undefined") {
-      index = previouslyInStock.findIndex((el) => el.id === sizeID);
+        let size = await product.variants.find((el) => el.id === sizeID);
+
+        let index = previouslyInStock.findIndex((e) => e.id === sizeID);
+
+        if (previouslyInStock.find((el) => el.id === sizeID)) {
+            if (previouslyInStock[index].available != size.available) {
+                previouslyInStock[index].available = size.available;
+            } else {
+                console.log("still out of stock");
+            }
+        } else {
+            console.log("adding to object");
+
+            previouslyInStock.push({ id: sizeID, available: size.available });
+            index = previouslyInStock.findIndex((e) => e.id === sizeID);
+        }
+
+        if (size.available && !previouslyInStock[index].available) {
+            await discordMessage();
+        }
+    } catch (err) {
+        console.log(err);
     }
-
-    // console.log(previouslyInStock);
-
-    if (previouslyInStock === [] || test === undefined) {
-      console.log("NEW");
-      previouslyInStock.push({ id: sizeID, available: false });
-      index = previouslyInStock.findIndex((e) => e.id === sizeID);
-      // console.log(index);
-    }
-
-    if (size.available) {
-      if (!previouslyInStock[index].available) {
-        console.log("INSTOCK");
-        await discordMessage();
-        previouslyInStock[index].available = size.available;
-      }
-    } else {
-      console.log("OOS");
-      if (previouslyInStock[index].available === false) {
-        previouslyInStock[index].available = size.available;
-      }
-    }
-  } catch (err) {
-    console.log(err);
-  }
 }
 
 async function discordMessage() {
-  const channel = client.channels.cache.get("891751964545781810");
+    const channel = client.channels.cache.get("891751964545781810");
 
-  const embed = new MessageEmbed()
-    .setColor("#fd2973")
-    .setTitle("Product In Stock")
-    .setURL(
-      `https://threadbare.com/products/mens-bloomfield-black-plain-cargo-trousers`
-    )
-    .setTimestamp()
-    .setFooter({
-      text: "Made by Roo#7777",
-      iconURL:
-        "https://i.ibb.co/VDMp2Bx/0e58a19b5a24f0542691313ff5106e40-1.png",
-    });
+    const embed = new MessageEmbed()
+        .setColor("#fd2973")
+        .setTitle("Product In Stock")
+        .setURL(`https://www.33-mm.com/products/royce-jumper-light-grey`)
+        .setTimestamp()
+        .setFooter({
+            text: "Made by Roo#7777",
+            iconURL:
+                "https://i.ibb.co/VDMp2Bx/0e58a19b5a24f0542691313ff5106e40-1.png",
+        });
 
-  channel.send({ content: "<@181094829500006400>", embeds: [embed] });
+    channel.send({ content: "<@181094829500006400>", embeds: [embed] });
 }
 
 client.login(process.env.DISCORD_TOKEN);
